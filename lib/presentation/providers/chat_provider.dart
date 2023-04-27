8556656f51a72a5b6f7d46b985b562784a8caae2
Angel Vargas <Yesid1031@hotmail.com>
2023-04-27 07:23:25 -0500

    Mappers

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,11 +1,13 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_petition.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
 
   final ScrollController chatScrollController = ScrollController();
+  final getPetition = Getpetition();
 
 List<Message> messageList = [
   Message(text: 'Hola amor', fromWho: FromWho.me),
@@ -16,8 +18,19 @@ Future<void> sendMessage(String text)async{
   if(text.isEmpty) return;
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
+
+    if(text.endsWith('?')){
+       herReply();
+    }
+
     notifyListeners();
     moveSrollToBottom();
+}
+
+
+Future<void> herReply()async{
+final herMessage = await getPetition.getAnswer();
+
 }
 
 Future<void> moveSrollToBottom ()async{
